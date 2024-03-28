498f256c803ca5d2b99a68b3c8f30fd11610c283
Roman Romanov <rromanov.vk@gmail.com>
2024-03-29 00:40:08 +0500

    init

---
+++ MovieQuiz/Helpers/Date+Extensions.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Date {
+    var dateTimeString: String { DateFormatter.defaultDateTime.string(from: self) }
+}
+
+private extension DateFormatter {
+    static let defaultDateTime: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.YY hh:mm"
+        return dateFormatter
+    }()
+}
